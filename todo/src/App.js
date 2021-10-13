3d9b5751e92899b59369949d1196453d23cc94ab
JRotko <76393423+JRotko@users.noreply.github.com>
2021-10-13 21:15:37 +0300

    todo with delete done

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react';
 import './App.css';
-import Todotable from './components/Todolist'
+import TodoList from './components/Todolist'
 
 function App() {
-  const [todo, setTodo] = useState({desc: '', date:''});
-  const [todos, setTodos] = useState([]);
 
-  const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, todo])
-  }
-
-    const inputChenged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  }
 
   return (
     <div className="App">
-      <form onSubmit={addTodo}>
-      <input type='date' name='date' value={todo.date} onChange={inputChenged}/>
-        <input type='text' name='desc' value={todo.desc} onChange={inputChenged}/>
-        <input type='submit' value='Add' />
-      </form>
-      <Todotable todos={todos} />
+      <TodoList />
     </div>
   );
 }
--- todo/src/components/Todolist.js
+++ todo/src/components/Todolist.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Todolist(props) {
+
+export default function TodoList() {
+
+    const [todo, setTodo] = useState({desc: '', date:''});
+    const [todos, setTodos] = useState([]);
+  
+    const addTodo = (event) => {
+      event.preventDefault();
+      setTodos([...todos, todo])
+    }
+  
+    const inputChenged = (event) => {
+      setTodo({...todo, [event.target.name]: event.target.value});
+    }
+
+    const deleteTodo = (rowIndex) => {
+        setTodos(todos.filter((todo, index) => index !== rowIndex))
+    }
+
     return(
         <div>
+            <form onSubmit={addTodo}>
+                <input type='date' name='date' value={todo.date} onChange={inputChenged}/>
+                <input type='text' name='desc' value={todo.desc} onChange={inputChenged}/>
+                <input type='submit' value='Add' />
+            </form>
             <table>
                 <tbody>
                     <tr>
@@ -10,10 +33,11 @@ export default function Todolist(props) {
                         <th>Date</th>
                     </tr>
                     {
-                    props.todos.map((todo, index) => 
+                    todos.map((todo, index) => 
                     <tr key={index}>
                         <td>{todo.desc}</td>
                         <td>{todo.date}</td>
+                        <td><input type='button' onClick={() => deleteTodo(index)} value='Delete'></input></td>
                     </tr>
                     )
                     }
@@ -22,3 +46,5 @@ export default function Todolist(props) {
         </div>
     )
 }
+
+
